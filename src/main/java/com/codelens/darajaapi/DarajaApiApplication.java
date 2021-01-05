bab4ce77386aa90224923bb288053fd9e4dabe01
Eric Gitonga Njue <eric.gitonga@imbank.co.ke>
2021-01-05 07:22:51 +0300

    Project & Environment SetUp

---
+++ src/main/java/com/codelens/darajaapi/DarajaApiApplication.java
@@ -0,0 +1,13 @@
+package com.codelens.darajaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarajaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarajaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codelens/darajaapi/DarajaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelens.darajaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarajaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
